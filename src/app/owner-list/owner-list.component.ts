be29b3d80a7ea8bc6c83c6fa477d670aef658379
Zusoy <gregoire.drapeau79@gmail.com>
2019-04-04 09:28:28 +0200

    Owner deletion

--- src/app/owner-list/owner-list.component.ts
+++ src/app/owner-list/owner-list.component.ts
@@ -5,7 +5,8 @@ import { Owner } from 'src/models/Owner';
 @Component({
   selector: 'owner-list',
   templateUrl: './owner-list.component.html',
-  styleUrls: ['./owner-list.component.css']
+  styleUrls: ['./owner-list.component.css'],
+  providers: [OwnerService]
 })
 export class OwnerListComponent implements OnInit 
 {
@@ -17,6 +18,12 @@ export class OwnerListComponent implements OnInit
   ) 
   { 
     this.owners = ownerService.models; //get service owners
+  }
+
+
+  deleteOwner(owner: Owner)
+  {
+    this.ownerService.remove(owner.id);
   }
 
   ngOnInit() {
